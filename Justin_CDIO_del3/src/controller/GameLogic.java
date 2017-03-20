6edfefcb4d5ca98b6b3d7402881d9f8077a295d7
jufa2401 <jufa2401@gmail.com>
2017-03-20 13:38:06 +0100

    TaxController

--- Justin_CDIO_del3/src/controller/GameLogic.java
+++ Justin_CDIO_del3/src/controller/GameLogic.java
@@ -9,10 +9,9 @@ import entity.fieldclasses.Ownable;
 public class GameLogic{
 
 	public static void FieldRules(GUIHandler GUIh, LanguageHandler language, int fieldNumber, Field field, Player player) {
-		Ownable ofield = (Ownable) field;
-		if (ofield.getPrice() > 0) {
+		if (field.getPrice() > 0) {
 			//			hvis feltet kan ejes
-		
+			Ownable ofield = (Ownable) field; 
 			if (ofield.getOwner() != null) {
 				// Hvis der er en ejer af feltet
 				int paid = ofield.landOnField(player);
@@ -30,34 +29,9 @@ public class GameLogic{
 				}
 			}
 		} else {
-			//			hvis feltet ikke kan ejes
-			int taxAmount = field.getTaxAmount();
-			int taxRate   = field.getTaxRate();
-			int paid;
-			// Tax-felt med valg af beløb håndteres specielt, spilleren spørges
-			if (taxAmount > 0 && taxRate > 0) {
-				//			Teksten på knapperne er ikke tekst, men tal og behøver derfor ikke oversættes
-				if (GUIh.getYesNo(language.askPayTax(), taxAmount+"", taxRate+"%")) {
-					// Fast beløb valgt
-					paid = field.landOnField(player);
-				} else {
-					// Rate valgt, beløb afhængig af saldobalance
-					paid = field.landOnField(player, taxRate);
-				}
-			} else {
-			// Normal fast Tax / bonus, betales uden der spørges
-				paid = field.landOnField(player);
-			}
-			// Der gives forskellig besked afhængig af om der betales skat, eller udbetales bonus
-			if (paid > 0) {
-				GUIh.getButtonPressed(language.playerTax(player.getName(), paid), language.Ok());
-			} else {
-				GUIh.getButtonPressed(language.playerBonus(player.getName(), -paid), language.Ok());
-			}
+			TaxController.TaxRules(GUIh, language, fieldNumber, field, player);
+
 		}
-		// GUI opdateres med den nye balance for spilleren
-		GUIh.setBalance(player.getName(), player.getBalance());
 
 	}
-
 }
--- Justin_CDIO_del3/src/controller/TaxController.java
+++ Justin_CDIO_del3/src/controller/TaxController.java
@@ -1,5 +1,42 @@
 package controller;
 
+import boundary.GUIHandler;
+import boundary.language.LanguageHandler;
+import entity.Player;
+import entity.fieldclasses.Field;
+
 public class TaxController {
+	public static void TaxRules(GUIHandler GUIh, LanguageHandler language, int fieldNumber, Field field, Player player) {
+		//			hvis feltet ikke kan ejes
+		int taxAmount = field.getTaxAmount();
+		int taxRate   = field.getTaxRate();
+		int paid;
+		// Tax-felt med valg af beløb håndteres specielt, spilleren spørges
+		if (taxAmount > 0 && taxRate > 0) {
+			//			Teksten på knapperne er ikke tekst, men tal og behøver derfor ikke oversættes
+			if (GUIh.getYesNo(language.askPayTax(), taxAmount+"", taxRate+"%")) {
+				// Fast beløb valgt
+				paid = field.landOnField(player);
+			} else {
+				// Rate valgt, beløb afhængig af saldobalance
+				paid = field.landOnField(player, taxRate);
+			}
+		} else {
+			// Normal fast Tax / bonus, betales uden der spørges
+			paid = field.landOnField(player);
+		}
+		// Der gives forskellig besked afhængig af om der betales skat, eller udbetales bonus
+		if (paid > 0) {
+			GUIh.getButtonPressed(language.playerTax(player.getName(), paid), language.Ok());
+		} else {
+			GUIh.getButtonPressed(language.playerBonus(player.getName(), -paid), language.Ok());
+		}	
+		// GUI opdateres med den nye balance for spilleren
+		GUIh.setBalance(player.getName(), player.getBalance());
+	}
 
 }
+
+
+
+
