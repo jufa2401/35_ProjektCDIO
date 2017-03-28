dd3c07af4fb0ec52dd54708099645f094c0768e5
Justin <jufa2401@gmail.com>
2017-03-28 12:58:54 +0200

    Empty implementations of DAO

--- Justin_CDIO_del3/src/controller/Connector.java
+++ Justin_CDIO_del3/src/controller/Connector.java
@@ -11,7 +11,7 @@ import java.sql.Statement;
 public class Connector {
     private final String HOST     = "Localhost";
     private final int    PORT     = 3306;
-    private final String DATABASE = "?";
+    private final String DATABASE = "matador";
     private final String USERNAME = "root"; 
     private final String PASSWORD = "";
     private Connection connection;
